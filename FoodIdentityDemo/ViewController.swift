a215953343fb2c0b785cfb37dccc96aecaabebe2
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-27 14:57:23 +0600

    Initial Commit

---
+++ FoodIdentityDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodIdentityDemo
+//
+//  Created by Joynal Abedin on 27/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
